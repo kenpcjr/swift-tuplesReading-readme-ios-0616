89ce26533334b4701c7b55d25f711d57cf82708c
kenpcjr <kennethcooke@me.com>
2016-07-10 08:03:14 -0400

    Done.

---
+++ Tuples-KC.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thisTuple: (String, Int) = ("Yep", 5)
+
+//thisTuple.1
+//
+//thisTuple.0
+
+var newTuple = (answer: "nope", amount: 10)
+
+newTuple.amount
+
+newTuple.answer
+
+newTuple.amount = 5
+
+print(newTuple)
+
+
